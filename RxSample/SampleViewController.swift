ac40be610be04ea9d19161d7cef152d335736b87
Takehiro Watanabe <takehiro.watanabe.224@gmail.com>
2019-04-29 12:14:39 +0900

    Add comment

---
+++ RxSample/SampleViewController.swift
@@ -0,0 +1,47 @@
+import UIKit
+import RxSwift
+
+class SampleViewController: UIViewController {
+    
+    private let disposeBag = DisposeBag()
+    private var viewModel: SampleViewModel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.viewModel = SampleViewModel()
+        
+        self.viewModel.helloWorldObservable
+            .subscribe(
+                // デフォルトのイベントを流す。イベント内に値を格納でき、何度でも呼び出せる。
+                onNext: { [weak self] value in
+                  print("value = \(value)")
+            })
+            // disposeBagを引数として渡すと
+            .disposed(by: disposeBag) // 購読を破棄
+        
+        self.viewModel.updateItem()
+    }
+    
+}
+
+class SampleViewModel {
+    
+    /*
+     Observable: 「観測可能」なものを表現し、イベントを検知するためのクラス
+     ストリームとも表現される
+     */
+    var helloWorldObservable: Observable<String> {
+        return helloWorldSubject.asObserver()
+    }
+    
+    private let helloWorldSubject = PublishSubject<String>()
+    
+    func updateItem() {
+        helloWorldSubject.onNext("Hello World!")
+        helloWorldSubject.onNext("Hello World!!")
+        helloWorldSubject.onNext("Hello World!!!")
+        helloWorldSubject.onCompleted()
+    }
+    
+}
--- RxSample/ViewController.swift
+++ RxSample/ViewController.swift
@@ -1,20 +1,43 @@
-//
-//  ViewController.swift
-//  RxSample
-//
-//  Created by 渡邊丈洋 on 2019/04/29.
-//  Copyright © 2019 渡邊丈洋. All rights reserved.
-//
-
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
+    
+    private let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 1.Subjectを定義
+        let helloWorldSubject = PublishSubject<String>()
+        
+        // 2.Subjectを購読
+        helloWorldSubject.subscribe(
+            // 3.値が流れてきたらprint()で値を出力するように定義
+            onNext: { message in
+            print("onNext: \(message)")
+        }, onError: { error in
+            // エラーイベント
+            // 1度だけ呼ばれ、その時点で終了、購読を破棄
+            print("onError: \(error)")
+        },
+           onCompleted: {
+            // 完了イベント
+            // 1度だけ呼ばれ、その時点で終了、購読を破棄
+            print("onCompleted")
+        },
+           onDisposed: {
+            print("onDisposed")
+        })
+            // 定義したクラスが破棄されたら、購読も自動的に破棄させる
+            .disposed(by: disposeBag)
+        
+        helloWorldSubject.onNext("Hello World!")
+        helloWorldSubject.onNext("Hello World!!")
+        helloWorldSubject.onNext("Hello World!!!")
+        helloWorldSubject.onCompleted()
     }
-
 
 }
 
