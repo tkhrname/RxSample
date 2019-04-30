a3090097a0c9ba708de983fcc7f4ce05c2261808
Takehiro Watanabe <takehiro.watanabe.224@gmail.com>
2019-04-30 21:48:45 +0900

    Add operator sample

--- RxSample/SampleViewController.swift
+++ RxSample/SampleViewController.swift
@@ -8,6 +8,7 @@ class SampleViewController: UIViewController {
     
     @IBOutlet weak var nameTextField: UITextView!
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var showUserNameButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,9 +35,55 @@ class SampleViewController: UIViewController {
         
         // subscribeを利用
         self.nameTextField.rx.text.subscribe(onNext: { text in
-            //
             self.nameLabel.text = text
         }).disposed(by: self.disposeBag)
+    }
+    
+    // Operatorサンプル1
+    private func mapSample1() {
+        self.nameTextField.rx.text
+            .map { text -> String in
+                return "\(text!.count)"
+            }
+            .bind(to: self.nameLabel.rx.text)
+            .disposed(by: self.disposeBag)
+    }
+    
+    // Operatorサンプル2
+    private func mapSample2() {
+        struct User{
+            var name: String
+        }
+        let user = User(name: "takehiro")
+        self.showUserNameButton
+            .rx // Reactive化
+            .tap // ControlEvent
+            .map{ _ in
+                return user.name
+            } // Disposable 購読
+            .bind(to: self.nameLabel.rx.text)
+            .disposed(by: self.disposeBag)
+    }
+    
+    // Operatorサンプル3 filter
+    // 整数が流れるObservableから偶数のイベントのみに絞り混んでeventObservableに流す
+    private func fileterSample() {
+        let subject = PublishSubject<Int>()
+        subject
+            .filter{ $0 % 2 == 0 }
+            .bind(to: subject.asObserver())
+            .disposed(by: self.disposeBag)
+    }
+    
+    // 複数のAPIにリクエストして同時に反映したい場合に使用することがある
+    private func zipSample() {
+        let api1Observable = PublishSubject<Int>().asObserver()
+        let api2Observable = PublishSubject<Int>().asObserver()
+        Observable.zip(api1Observable, api2Observable)
+            .subscribe(onNext: { (api1, api2) in
+                //
+            })
+            .disposed(by: self.disposeBag)
     }
     
 }
