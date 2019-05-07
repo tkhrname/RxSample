65839d488827a1cef7047c3945207fec43998b67
Takehiro Watanabe <takehiro.watanabe.224@gmail.com>
2019-05-07 11:55:29 +0900

    Add CounterViewModel

---
+++ RxSample/BaseViewController.swift
@@ -0,0 +1,43 @@
+import UIKit
+import RxSwift
+import RxCocoa
+
+class BaseViewController: UIViewController {
+    
+    private let disposeBag = DisposeBag()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 1.Subjectを定義
+        let helloWorldSubject = PublishSubject<String>()
+        
+        // 2.Subjectを購読
+        helloWorldSubject.subscribe(
+            // 3.値が流れてきたらprint()で値を出力するように定義
+            onNext: { message in
+            print("onNext: \(message)")
+        }, onError: { error in
+            // エラーイベント
+            // 1度だけ呼ばれ、その時点で終了、購読を破棄
+            print("onError: \(error)")
+        },
+           onCompleted: {
+            // 完了イベント
+            // 1度だけ呼ばれ、その時点で終了、購読を破棄
+            print("onCompleted")
+        },
+           onDisposed: {
+            print("onDisposed")
+        })
+            // 定義したクラスが破棄されたら、購読も自動的に破棄させる
+            .disposed(by: disposeBag)
+        
+        helloWorldSubject.onNext("Hello World!")
+        helloWorldSubject.onNext("Hello World!!")
+        helloWorldSubject.onNext("Hello World!!!")
+        helloWorldSubject.onCompleted()
+    }
+
+}
+
--- RxSample/ViewController.swift
+++ RxSample/ViewController.swift
@@ -1,43 +1,49 @@
+//
+//  ViewController.swift
+//  RxSample
+//
+//  Created by 渡邊丈洋 on 2019/05/07.
+//  Copyright © 2019 渡邊丈洋. All rights reserved.
+//
+
 import UIKit
-import RxSwift
-import RxCocoa
 
 class ViewController: UIViewController {
-    
-    private let disposeBag = DisposeBag()
 
+    @IBOutlet weak var countLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // 1.Subjectを定義
-        let helloWorldSubject = PublishSubject<String>()
-        
-        // 2.Subjectを購読
-        helloWorldSubject.subscribe(
-            // 3.値が流れてきたらprint()で値を出力するように定義
-            onNext: { message in
-            print("onNext: \(message)")
-        }, onError: { error in
-            // エラーイベント
-            // 1度だけ呼ばれ、その時点で終了、購読を破棄
-            print("onError: \(error)")
-        },
-           onCompleted: {
-            // 完了イベント
-            // 1度だけ呼ばれ、その時点で終了、購読を破棄
-            print("onCompleted")
-        },
-           onDisposed: {
-            print("onDisposed")
-        })
-            // 定義したクラスが破棄されたら、購読も自動的に破棄させる
-            .disposed(by: disposeBag)
-        
-        helloWorldSubject.onNext("Hello World!")
-        helloWorldSubject.onNext("Hello World!!")
-        helloWorldSubject.onNext("Hello World!!!")
-        helloWorldSubject.onCompleted()
-    }
 
+    }
+    
+    @IBAction func countUp(_ sender: Any) {
+    }
+    
+    @IBAction func countDown(_ sender: Any) {
+    }
+    
+    @IBAction func countReset(_ sender: Any) {
+    }
+    
 }
 
+class CounterViewModel {
+    private(set) var count = 0
+    
+    func incrementCount(callBack: (Int) -> ()) {
+        count += 1
+        callBack(count)
+    }
+    
+    func decrementCount(callBack: (Int) -> ()) {
+        count -= 1
+        callBack(count)
+    }
+    
+    func resetCount(callBack: (Int) -> ()) {
+        count -= 1
+        callBack(count)
+    }
+    
+}
