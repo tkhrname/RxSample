bc01eff1e67986cefe71060f97a631a93be03c7a
Takehiro Watanabe <takehiro.watanabe.224@gmail.com>
2019-05-30 15:20:30 +0900

    Add test

--- RxSampleTests/RxSampleTests.swift
+++ RxSampleTests/RxSampleTests.swift
@@ -1,11 +1,3 @@
-//
-//  RxSampleTests.swift
-//  RxSampleTests
-//
-//  Created by 渡邊丈洋 on 2019/04/29.
-//  Copyright © 2019 渡邊丈洋. All rights reserved.
-//
-
 import XCTest
 @testable import RxSample
 
@@ -15,11 +7,9 @@ class RxSampleTests: XCTestCase {
     var fakePresenter: FakeSearchEventPresenter!
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
     func testExample() {
@@ -71,11 +61,8 @@ class RxSampleTests: XCTestCase {
         }
     }
     
-
     func testPerformanceExample() {
-        // This is an example of a performance test case.
         self.measure {
-            // Put the code you want to measure the time of here.
         }
     }
 
